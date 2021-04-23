9c020a271165fc162b6b5a9a89c91d5a64d9f3b4
pony <pony321@163.com>
2021-04-23 14:41:22 +0800

    jar包升级

--- component/common/lastestpom.sh
+++ component/common/lastestpom.sh
@@ -1,2 +1,24 @@
-cd ..
 mvn versions:use-latest-releases -DprocessAllModules=true
+
+
+
+
+cd component
+cd mysql
+mvn versions:use-latest-releases
+
+cd ..
+cd redis
+mvn versions:use-latest-releases
+
+cd ..
+cd webflux
+mvn versions:use-latest-releases
+
+cd ..
+cd dubbo
+mvn versions:use-latest-releases
+
+cd ..
+cd common
+mvn versions:use-latest-releases
