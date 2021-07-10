5d7a8652fba18dad557e699aa56154ea181ac271
zane <zane>
2021-07-10 19:39:09 +0800

    broad

--- pages/vue-demo/views/projects/chat/where.js
+++ pages/vue-demo/views/projects/chat/where.js
@@ -1,9 +1,12 @@
 /**获取场所*/
 function getWhere() {
     let list = [];
+    // 获取租户客服
     list.push({"img": "kefu.jpeg", "name": "客服", "type": "customer", "area": "cus", "id": 1});
+    // 获取租户默认群组
     list.push({"img": "who.jpg", "name": "系统群", "type": "group", "area": "sys", "id": 2});
     for (let i = 10; i < 20; i++) {
+        // 获取租户默认群组
         list.push({"img": "who.jpg", "name": "私聊"+i, "type": "friend", "area": "pri", "id":  i});
     }
     return list;
@@ -15,7 +18,7 @@ function listen() {
     let places = "";
     for (let i = 0; i < list.length; i++) {
         let place = list[i]['id'];
-        if( list[i]['type']==="friend" || list[i]['type']==="customer" ){
+        if( list[i]['type']!=="group" ){
             place=Math.min(list[i]['id'],userId)+"@"+Math.max(list[i]['id'],userId);
         }
         places += place + ",";
@@ -32,11 +35,10 @@ var activePlace;
 
 /**打开场所*/
 function active(who,userId) {
-    console.log(who,userId);
     activePlace = who['id'];
     let msg = document.querySelector('#chat_msg');
     let place = who['id'];
-    if( who['type']==="friend" || who['type']==="customer" ){
+    if( list[i]['type']!=="group" ){
         place=Math.min(who['id'],userId)+"@"+Math.max(who['id'],userId);
     }
     msg.innerHTML = '<div id="chat_msg_who" style="border-bottom: 1px solid rgba(0,0,0,0.2);">' +
