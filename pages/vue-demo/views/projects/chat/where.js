395fe410a6d5d19a7980637ae0667061e42b35c6
zane <zane>
2021-07-12 15:22:03 +0800

    broad

--- pages/vue-demo/views/projects/chat/where.js
+++ pages/vue-demo/views/projects/chat/where.js
@@ -1,13 +1,15 @@
 /**获取场所*/
 function getWhere() {
     let list = [];
+    // 系统设置客服/默认群，用户默认群
     list.push({"img": "kefu.jpeg", "name": "客服", "type": "customer", "area": "cus", "id": 1});
-    list.push({"img": "who.jpg", "name": "系统群", "type": "group", "area": "cus", "id": 2});
+    list.push({"img": "who.jpg", "name": "系统群", "type": "group", "area": "cus", "id": 2,"forbiden":true});
     for (let i = 10; i < 20; i++) {
         list.push({"img": "who.jpg", "name": "私聊" + i, "type": "friend", "area": "pri", "id": i});
     }
     return list;
 }
+/**被禁言*/
 
 /**关注场所消息*/
 function listen() {
@@ -35,7 +37,7 @@ function active(who, userId) {
     activePlace = who['id'];
     let msg = document.querySelector('#chat_msg');
     let place = who['id'];
-    if (list[i]['type'] !== "group") {
+    if (who['type'] !== "group") {
         place = Math.min(who['id'], userId) + "@" + Math.max(who['id'], userId);
     }
     msg.innerHTML = '<div id="chat_msg_who" style="border-bottom: 1px solid rgba(0,0,0,0.2);">' +
